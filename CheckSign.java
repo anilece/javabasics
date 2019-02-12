52b258b764f73b1d1035986db20ecf7c2b297840
anilece <47322405+anilece@users.noreply.github.com>
2019-02-12 18:41:18 +0530

    Update CheckSign.java

--- CheckSign.java
+++ CheckSign.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 class CheckSign{
  public static void main(String[] args){ 
-   Scanner scan=new Scanner(System.in);
-   int i=scan.nextInt();
-   if (i>0){
+   Scanner sc=new Scanner(System.in);
+   int i;
+   if (!sc.hasNextInt()) {
+        System.out.println("Invalid Input");
+        sc.next(); 
+    }
+	  else{
+        i = sc.nextInt();
+   
+  
+    if (i>0){
       System.out.print("Positive");
-   }
+      }
    else if(i==0){
       System.out.print("Zero");
-   }
+      }
    else{
       System.out.print("Negative");
-   }
+      }
+   }	   
  }
-}
+ }
